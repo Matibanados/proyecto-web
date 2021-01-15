426e85aa2b2265c87fac2e514e7de76fc7372943
Matías Bañados <matiasbanados@iMac-de-Matias.local>
2021-01-14 23:45:06 -0300

    Primera subida web

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+
+function masinfo(){
+    alert("Este contenido aún no está disponible, disculpe las molestias");
+}
+
+function nodisponible(){
+    alert("El formulario no se encuentra disponible.");
+}
